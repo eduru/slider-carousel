9e2ea2c187f8b8dab023a41f3d869a9a57e32230
Eduardo Ruiz <eduru@me.com>
2021-10-01 11:51:17 +0900

    adding logic to buttons

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -6,10 +6,22 @@ const ImageSlider = ({ slides }) => {
   const [current, setCurrent] = useState(0);
   const length = slides.length;
 
+  const prevSlide = () => {
+    setCurrent(current === 0 ? 5 : current - 1);
+  };
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  if (!Array.isArray(slides) || length <= 0) {
+    return null;
+  }
+
   return (
     <section className="slider">
-      <FaArrowAltCircleLeft className="left-arrow" />
-      <FaArrowAltCircleRight className="right-arrow" />
+      <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
+      <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
       {SliderData.map((s) => (
         <img className="image" src={s.image} alt={s.alt} />
       ))}
